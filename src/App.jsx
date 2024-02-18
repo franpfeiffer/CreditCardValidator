f7d7bdafa12521b2b928a9f3823c4c838d18ef85
Francisco Pfeiffer <pfeifferfran1@gmail.com>
2024-02-18 15:16:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CreditCardForm from './components/CreditCardForm.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+      <CreditCardForm />
+    </div>
+  )
+}
+
+export default App
